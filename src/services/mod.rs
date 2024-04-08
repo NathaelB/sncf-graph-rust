e39dc33f95a8c5a9536adb01afae88f6e4364815
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2024-04-08 15:43:07 +0200

    feat(service): mod stop_time_service

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,1 +1,2 @@
 pub mod stop_service;
+pub mod stop_time_service;
--- src/services/stop_service.rs
+++ src/services/stop_service.rs
@@ -1,12 +1,19 @@
-use actix_web::web;
+use actix_web::{web};
 use futures::TryStreamExt;
-use mongodb::Collection;
 use crate::models::stop::Stop;
 use crate::pagination::{PaginationBuilder, PaginationResponse};
+
+use mongodb::{
+   bson::{extjson::de::Error, oid::ObjectId},
+   Collection
+};
 
 pub struct StopService {
    pub collection: web::Data<Collection<Stop>>
 }
+
+
+
 
 impl StopService {
    pub async fn find_all(&self, page: i64, size: i64, base_url: &str) -> Result<PaginationResponse<Stop>, mongodb::error::Error> {
@@ -26,5 +33,18 @@ impl StopService {
 
       let builder = PaginationBuilder::new(data, total, page, size, base_url.to_string());
       Ok(builder.build_response())
+   }
+
+   pub async fn find_by_id(&self, id: &str) -> Result<Option<Stop>, Error>{
+      let obj_id = ObjectId::parse_str(id)?;
+
+
+
+      let filter = mongodb::bson::doc! {"_id": obj_id};
+      let stop = self.collection.find_one(filter, None)
+          .await.ok()
+          .expect("Error getting stop's detail");
+
+      Ok(stop)
    }
 }
