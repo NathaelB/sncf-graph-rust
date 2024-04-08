5490033bc7086bc366881bff5c26d8af37ab5615
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2024-04-08 15:43:25 +0200

    feat(model): export model and pub create for Stop struct

--- src/models/stop.rs
+++ src/models/stop.rs
@@ -1,14 +1,17 @@
+use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Default, Clone)]
+#[derive(Serialize, Deserialize, Default, Clone, Debug)]
 pub struct Stop {
-    stop_id: String,
-    stop_name: String,
-    stop_desc: Option<String>,
-    stop_lat: f64,
-    stop_lon: f64,
-    zone_id: Option<String>,
-    stop_url: Option<String>,
-    location_type: String,
-    parent_station: Option<String>
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<ObjectId>,
+    pub stop_id: String,
+    pub stop_name: String,
+    pub stop_desc: Option<String>,
+    pub stop_lat: f64,
+    pub stop_lon: f64,
+    pub zone_id: Option<String>,
+    pub stop_url: Option<String>,
+    pub location_type: String,
+    pub parent_station: Option<String>
 }
